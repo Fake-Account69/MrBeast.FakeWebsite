000f1e6bc1d910c8411671c9ec714e0c4bce088a
Fake-Account69 <100270145+Fake-Account69@users.noreply.github.com>
2022-02-23 13:32:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function fun(l) {
     dateformat.i18n = require('./lang/' + l);
     return true;
 }
+
+var fun = function lang(l) {
+    dateformat.i18n = require('./lang/' + l)
+    return true;
+  }
